3fd1b84fef06b263c85a5272a8951bcb17770d15
Faisal Malik <faisalmalik@Faisals-MacBook-Pro.local>
2025-01-23 19:34:56 +0500

    Fixed recent checkins time issue

--- src/Components/ChildAdmin/Dashboard/RecentLabel.jsx
+++ src/Components/ChildAdmin/Dashboard/RecentLabel.jsx
@@ -1,34 +1,45 @@
 import React from 'react'
 
-const RecentLabel = ({data}) => {
+const RecentLabel = ({ data }) => {
   const formatDateTime = (dateString, timeString) => {
     const date = new Date(dateString);
-  
+
     // Format the date as "Month day Year"
     const formattedDate = date.toLocaleDateString('en-US', {
       month: 'short', // 'Oct'
       day: '2-digit', // '25'
       year: 'numeric' // '2024'
     }).replace(/,/, ''); // Remove the comma after the month and day
-  
+
+
     // Format the time as "HH:MM AM/PM"
-    const formattedTime = new Date(`1970-01-01T${timeString}Z`).toLocaleTimeString('en-US', {
+    console.log("Time before Format is :", timeString);
+    date.setHours(parseInt(timeString.substring(0, 2)));
+    date.setMinutes(parseInt(timeString.substring(3, 5)));
+    date.setSeconds(parseInt(timeString.substring(6)));
+
+    // Format the time using toLocaleTimeString
+    const formattedTime = date.toLocaleTimeString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
-      hour12: true // Display in 12-hour format
+      hour12: true
     });
-  
+    console.log("Formatted Time is :", formattedTime);
+
+
     return `${formattedDate}, ${formattedTime}`;
   };
-  const entry=formatDateTime(data.checkinDate ,data.checkinTime )
+  const entry = formatDateTime(data.checkinDate, data.checkinTime)
+  console.log("Entry is :", data.checkinTime);
+
   return (
-    <div className="my-3 p-3 d-flex justify-content-between" style={{border:'1px solid #000000', borderRadius:'15px'}}>
-        <div className="name poppins-regular" style={{fontSize:'16px'}}>
+    <div className="my-3 p-3 d-flex justify-content-between" style={{ border: '1px solid #000000', borderRadius: '15px' }}>
+      <div className="name poppins-regular" style={{ fontSize: '16px' }}>
         {data.Athlete.name}
-        </div>
-        <div className="time poppins-regular" style={{fontSize:'16px'}}>
+      </div>
+      <div className="time poppins-regular" style={{ fontSize: '16px' }}>
         {entry}
-        </div>
+      </div>
     </div>
   )
 }
